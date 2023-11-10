ea14c40c9e75bc3f5873993231f0627d3c9c80d8
Misha676767 <150454540+Misha676767@users.noreply.github.com>
2023-11-10 19:34:03 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // вывод в консоль приведствия
+    void Start()
+    {
+        print("Hello, World!");
+    }
+
+}
